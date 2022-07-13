c6b028c419836e15951da30bacecba7dc564abc4
Brian <bmaitner@gmail.com>
2022-07-13 11:58:11 -0400

    moving rmd files to a common directory to keep things neat

--- _targets.R
+++ _targets.R
@@ -42,36 +42,37 @@ list(
 
     tar_target(name = model_prediction,
                  command = get_model_data(file = "model_prediction.rds")
-    ),
-
-
-  tar_target(name = parks,
-             command = get_park_polygons(temp_directory = "data/temp/",
-                                         sacad_filename = "data/manual_downloads/protected_areas/SACAD_OR_2021_Q4.shp",
-                                         sapad_filename = "data/manual_downloads/protected_areas/SAPAD_OR_2021_Q4.shp",
-                                         cape_nature_filename = "data/manual_downloads/protected_areas/Provincial_Nature_Reserves/CapeNature_Reserves_gw.shp")
-             ),
-
-  tar_target(name = ndwi,
-             command = get_release_ndwi_modis(temp_directory = "data/temp/raw_data/NDWI_MODIS/",
-                                              tag = "current"))
-  ,
-
-
-  tar_target(name = noaa_data,
-             command = update_climate_data(parks = parks,
-                                           temp_directory = "data/temp/noaa",
-                                           sleep_time = 30,
-                                           max_attempts = 10,
-                                           reset_all = FALSE)), #set this to TRUE to re-download everything, rather than only updating
-
- tar_target(name = reports,
-            command = generate_reports(output_directory = "reports/",
-                                       temp_directory = "data/temp/reports/",
-                                       report_location = "report_prototype.rmd",
-                                       time_window_days = 365,
-                                       n_stations = 3,
-                                       parks = parks,
-                                       ... = noaa_data,
-                                       ... = ndwi))
+    )
+ #    ,
+ #
+ #
+ #  tar_target(name = parks,
+ #             command = get_park_polygons(temp_directory = "data/temp/",
+ #                                         sacad_filename = "data/manual_downloads/protected_areas/SACAD_OR_2021_Q4.shp",
+ #                                         sapad_filename = "data/manual_downloads/protected_areas/SAPAD_OR_2021_Q4.shp",
+ #                                         cape_nature_filename = "data/manual_downloads/protected_areas/Provincial_Nature_Reserves/CapeNature_Reserves_gw.shp")
+ #             ),
+ #
+ #  tar_target(name = ndwi,
+ #             command = get_release_ndwi_modis(temp_directory = "data/temp/raw_data/NDWI_MODIS/",
+ #                                              tag = "current"))
+ #  ,
+ #
+ #
+ #  tar_target(name = noaa_data,
+ #             command = update_climate_data(parks = parks,
+ #                                           temp_directory = "data/temp/noaa",
+ #                                           sleep_time = 30,
+ #                                           max_attempts = 10,
+ #                                           reset_all = FALSE)), #set this to TRUE to re-download everything, rather than only updating
+ #
+ # tar_target(name = reports,
+ #            command = generate_reports(output_directory = "reports/",
+ #                                       temp_directory = "data/temp/reports/",
+ #                                       report_location = "report_prototype.rmd",
+ #                                       time_window_days = 365,
+ #                                       n_stations = 3,
+ #                                       parks = parks,
+ #                                       ... = noaa_data,
+ #                                       ... = ndwi))
 )
