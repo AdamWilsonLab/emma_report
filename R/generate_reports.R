ab6ea922bd351826f97ddc6afb39652d419eb282
Brian <bmaitner@gmail.com>
2024-03-11 12:55:35 -0400

    update to handle parks without any fires

--- R/generate_reports.R
+++ R/generate_reports.R
@@ -511,7 +511,8 @@ generate_reports <- function(output_directory = "reports/",
 
 
     # Generate the Cape Nature reports via a for loop
-      #park_name <- unique(parks$cape_nature$COMPLEX)[33]
+      # park_name <- unique(parks$cape_nature$COMPLEX)[33]
+      # park_name <- unique(parks$cape_nature$COMPLEX)[18]
 
     for (park_name in unique(parks$cape_nature$COMPLEX)){
 
