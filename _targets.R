cf51dfb656c71635c60b1f66b3cb948335f256dc
Jiyeon Kim <59989607+jiyeon-kim20@users.noreply.github.com>
2025-09-11 13:50:16 -0400

    Update _targets.R

--- _targets.R
+++ _targets.R
@@ -78,10 +78,15 @@ list(
               ),
 
 
-  tar_target(env_files,
-              command = get_env_files(),
-              age     = as.difftime(1, units = "days") # check
-              ),
+  # tar_target(env_files,
+  #             command = get_env_files()
+  #             ),
+
+  tar_age(
+        name    = env_files,
+        command = get_env_files(),
+        age     = as.difftime(1, units = "days")   # 하루마다 목록 새로 가져오기
+      )
   #   tar_target(
   #   env_files,
   #   # command = get_env_files(), 
