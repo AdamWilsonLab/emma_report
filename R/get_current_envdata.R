f319f1fe5d2f27529f5bd9d78197ed6c44ea17ef
Jiyeon Kim <59989607+jiyeon-kim20@users.noreply.github.com>
2025-09-11 23:39:02 -0400

    Update get_current_envdata.R

--- R/get_current_envdata.R
+++ R/get_current_envdata.R
@@ -1,8 +1,4 @@
-nvdata.R
-+295
-Lines changed: 295 additions & 0 deletions
-Original file line number	Diff line number	Diff line change
-@@ -0,0 +1,295 @@
+
 library(stars)
 library(tidyverse)
 library(lubridate)
